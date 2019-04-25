c2d4c815a35074e8d882d30df018b375a6ad94f9
urbanojvr <u.villanueva95@gmail.com>
2019-04-25 02:49:01 +0200

    - DocumentLoader implemented (url and file) WITH TESTS - HtmlParser implemented WITH TESTS
    pom updated with hamcrest and junit
    
    gitignore updated to ignore target folder

--- src/main/java/com/urbanojvr/jsoupproofs/Main.java
+++ src/main/java/com/urbanojvr/jsoupproofs/Main.java
@@ -1,7 +1,16 @@
 package com.urbanojvr.jsoupproofs;
 
+import com.urbanojvr.jsoupproofs.htmlparser.HtmlParser;
+import org.jsoup.nodes.Document;
+
 public class Main {
     public static void main(String[] args){
+        String text = "<html><head><title>Proof of concept</title></head>"
+                + "<body><p>Parsing String into a HTML document</p>" +
+                "<p>JSOUP PROOF OFO CONCEPT</p></body></html>";
 
+        HtmlParser parser = new HtmlParser(text);
+        Document doc = parser.getDoc();
+        System.out.println(doc.body());
     }
 }
